d7835827e584ee3ff0435a3e56debfea758928b2
Rutuja Yemle <163744661+rutujayemle17@users.noreply.github.com>
2024-07-25 16:36:59 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// JavaScript code can be added here if needed for additional functionalities or interactions.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript loaded.');
+});
